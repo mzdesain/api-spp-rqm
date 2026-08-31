128ec874d8f6cdf30c47d3938761d1079ca711b3
Muzanni <mzdesain.my.id@gmail.com>
2026-08-31 11:17:16 +0800

    Update aplikasi

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
 // Ganti nilai di dalam tanda kutip dengan URL Web App Google Apps Script kamu
-const API_URL = "https://script.google.com/macros/s/AKfycbxQ7sXjIZoLYzkvoj-6zxCNm4HFk5z6B6SMIi-_3MdbbZlcO_tPUyNtTfWO9JjIjR9n/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby9wVy0J9PgON44t0d_jOA878Zz30F6FUR5tBrnB2OJMB2xxwZHtgTL5Pj9Z4aEPdsD/exec";
